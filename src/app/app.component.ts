91b20b36d7d6ff6b715f6d7ba72e1dc2704147e6
Grazielle moraes <98981468+GrazielleDK13@users.noreply.github.com>
2022-09-23 12:32:00 -0300

    primeiro evento criado

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,31 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'appangular';
+
+  // <~~ FORMA SIMPLES MENOS VERBOSA ~~>
+
+  transferencia: any;
+
+  //FORMA ROBUSTA E VERBOSA
+
+  // destino!: number;
+  // valor!: number;
+
+  transferir($event: any) {
+    
+    console.log($event);
+
+    // <~~ FORMA SIMPLES MENOS VERBOSA ~~>
+
+    this.transferencia = $event;
+
+    //FORMA ROBUSTA E VERBOSA
+
+    // this.destino = $event.destino;
+    // this.valor = $event.valor;
+  }
 }
--- src/app/nova-transferencia/nova-transferencia.component.ts
+++ src/app/nova-transferencia/nova-transferencia.component.ts
@@ -1,22 +1,65 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 
 @Component({
   selector: 'app-nova-transferencia',
   templateUrl: './nova-transferencia.component.html',
   styleUrls: ['./nova-transferencia.component.css']
 })
+
 export class NovaTransferenciaComponent implements OnInit {
 
+  //atributos
+
+    // public valor  = '';
+    // public destino = '';
+
+    @Output() aoTranferir = new EventEmitter<any>();
+
+    // o @Output() serve para criar um evento
+
+    valor!: number;
+    destino!: number;
+
+
   // metodo
-  
+
   transferir(){
+
     console.log('Solicitada nova transferência');
 
+    const valorEmitir = { valor : this.valor, destino:this.destino};
+    this.aoTranferir.emit(valorEmitir);
+
+    this.limparCampos();
+
+
+    // console.log('valor: ', this.valor);
+    // console.log('Destino: ', this.destino);
+
   }
 
-  constructor() { }
+  limparCampos(){
+
+    this.valor = 0 ;
+    this.destino = 0 ;
+
+  }
+
+
+
+  constructor() {
+
+  }
+
+
 
   ngOnInit(): void {
+
+
   }
+
+
+
+
 
 }
